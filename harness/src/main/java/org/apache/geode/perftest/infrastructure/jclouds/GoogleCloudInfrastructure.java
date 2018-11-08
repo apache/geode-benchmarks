788413803a794d4e57eb5a0fa04690de9b6a0277
Pivotal <pivotal@example.com>
2018-11-08 15:29:26 -0800

    Implement copyFromNodes for google cloud.

--- harness/src/main/java/org/apache/geode/perftest/infrastructure/jclouds/GoogleCloudInfrastructure.java
+++ harness/src/main/java/org/apache/geode/perftest/infrastructure/jclouds/GoogleCloudInfrastructure.java
@@ -1,42 +1,51 @@
 package org.apache.geode.perftest.infrastructure.jclouds;
 
-import java.io.File;
-import java.io.IOException;
-import java.io.UncheckedIOException;
-import java.net.InetAddress;
-import java.net.UnknownHostException;
-import java.nio.charset.Charset;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.Set;
-
 import com.google.common.base.Supplier;
 import com.google.common.collect.ImmutableSet;
 import com.google.inject.Module;
 import org.apache.commons.io.FileUtils;
+import org.apache.geode.perftest.infrastructure.CommandResult;
+import org.apache.geode.perftest.infrastructure.Infrastructure;
 import org.jclouds.ContextBuilder;
 import org.jclouds.compute.ComputeService;
 import org.jclouds.compute.ComputeServiceContext;
 import org.jclouds.compute.domain.ExecResponse;
 import org.jclouds.compute.domain.NodeMetadata;
+import org.jclouds.compute.domain.NodeMetadataBuilder;
 import org.jclouds.compute.options.RunScriptOptions;
 import org.jclouds.domain.Credentials;
+import org.jclouds.domain.LoginCredentials;
 import org.jclouds.googlecloud.GoogleCredentialsFromJson;
-import org.jclouds.googlecomputeengine.GoogleComputeEngineApi;
-import org.jclouds.googlecomputeengine.domain.Metadata;
+import org.jclouds.io.Payload;
+import org.jclouds.io.payloads.FilePayload;
+import org.jclouds.ssh.SshClient;
 import org.jclouds.sshj.config.SshjSshClientModule;
 
-import org.apache.geode.perftest.infrastructure.CommandResult;
-import org.apache.geode.perftest.infrastructure.Infrastructure;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.UncheckedIOException;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
 
 public class GoogleCloudInfrastructure implements Infrastructure {
 
   public static final String CREDENTIALS_ENV = "GOOGLE_APPLICATION_CREDENTIALS";
   private final Set<GoogleCloudNode> nodes;
   private final ComputeService client;
+  private final ComputeServiceContext context;
 
   public GoogleCloudInfrastructure(int nodeCount) throws IOException {
+    final LoginCredentials sshCredentials;
+    final String key = FileUtils.readFileToString(new File(System.getProperty("user.home"), ".ssh/google_compute_engine"), Charset.defaultCharset());
 
     String credentialsFile = System.getenv(CREDENTIALS_ENV);
     if(credentialsFile == null) {
@@ -46,7 +55,7 @@ public class GoogleCloudInfrastructure implements Infrastructure {
     String fileContents = FileUtils.readFileToString(new File(credentialsFile), Charset.defaultCharset());
 
     Supplier<Credentials> credentials = new GoogleCredentialsFromJson(fileContents);
-    ComputeServiceContext context = ContextBuilder.newBuilder("google-compute-engine")
+    context = ContextBuilder.newBuilder("google-compute-engine")
         .modules(ImmutableSet.<Module> of(new SshjSshClientModule()))
         .credentialsSupplier(credentials)
         .buildView(ComputeServiceContext.class);
@@ -60,12 +69,13 @@ public class GoogleCloudInfrastructure implements Infrastructure {
     if(nodeMetadata.size() < nodeCount) {
       throw new IllegalStateException("Need at least " + nodeCount + " nodes. Have " + nodeMetadata.size());
     }
+    sshCredentials = LoginCredentials.builder().user("geode").privateKey(key).build();
 
     nodes = new HashSet<>();
     Iterator<? extends NodeMetadata> nodeItr = nodeMetadata.iterator();
     for(int i = 0; i < nodeCount; i++) {
       NodeMetadata metadata = nodeItr.next();
-      nodes.add(new GoogleCloudNode(metadata));
+      nodes.add(new GoogleCloudNode(NodeMetadataBuilder.fromNodeMetadata(metadata).credentials(sshCredentials).build()));
     }
   }
 
@@ -80,8 +90,7 @@ public class GoogleCloudInfrastructure implements Infrastructure {
 
     String script = String.join(" ", shellCommand);
 
-    String key = FileUtils.readFileToString(new File(System.getProperty("user.home"), ".ssh/id_rsa"), Charset.defaultCharset());
-    System.err.println("key=" + key);
+    String key = FileUtils.readFileToString(new File(System.getProperty("user.home"), ".ssh/google_compute_engine"), Charset.defaultCharset());
     RunScriptOptions
         runScriptOptions =
         new RunScriptOptions().overrideLoginPrivateKey(key).overrideLoginUser("geode");
@@ -98,16 +107,50 @@ public class GoogleCloudInfrastructure implements Infrastructure {
 
   @Override
   public void copyToNodes(Iterable<File> files, String destDir) throws IOException {
+    for(GoogleCloudNode node : nodes) {
+      SshClient ssh = context.utils().sshForNode().apply(node.getMetadata());
+      ssh.connect();
+      ssh.exec("mkdir " + destDir);
+      for(File file : files) {
+        ssh.put(destDir + "/" + file.getName(), new FilePayload(file));
+      }
+    }
 
   }
 
   @Override
   public void copyFromNode(Node node, String directory, File destDir) throws IOException {
+    SshClient ssh = context.utils().sshForNode().apply(((GoogleCloudNode)node).getMetadata());
+    ssh.connect();
+    destDir.mkdirs();
+    Path remoteDirectory = new File(directory).toPath();
+    Path remoteDirectoryName = remoteDirectory.getFileName();
+    Path remoteParent = remoteDirectory.getParent();
+    String remoteArchiveFilename = remoteDirectoryName.toString() + ".tar";
+    String remoteArchivePathname = remoteParent.toString() + "/" + remoteArchiveFilename;
+    String[] shellCommand = new String[] {
+      "tar", "zcvf", remoteArchivePathname, "-C", remoteParent.toString(), remoteDirectoryName.toString()
+      };
+    this.onNode(node,shellCommand);
+    Payload tarPayload = ssh.get(remoteArchivePathname);
+
+
+      InputStream tarStream = tarPayload.openStream();
+      File targetFile = new File(destDir.toString()+ "/" +remoteArchiveFilename.toString());
+
+      Files.copy(
+              tarStream,
+              targetFile.toPath(),
+              StandardCopyOption.REPLACE_EXISTING);
 
   }
 
   private class GoogleCloudNode implements Node {
     private NodeMetadata metadata;
+
+    public NodeMetadata getMetadata() {
+      return metadata;
+    }
 
     public GoogleCloudNode(NodeMetadata next) {
       this.metadata = next;
--- harness/src/test/java/org/apache/geode/perftest/infrastructure/jclouds/GoogleCloudInfrastructureTest.java
+++ harness/src/test/java/org/apache/geode/perftest/infrastructure/jclouds/GoogleCloudInfrastructureTest.java
@@ -2,16 +2,21 @@ package org.apache.geode.perftest.infrastructure.jclouds;
 
 import static org.junit.Assert.*;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
 
+import org.junit.Rule;
 import org.junit.Test;
 
 import org.apache.geode.perftest.infrastructure.CommandResult;
 import org.apache.geode.perftest.infrastructure.Infrastructure;
+import org.junit.rules.TemporaryFolder;
 
 public class GoogleCloudInfrastructureTest {
+  @Rule
+  public TemporaryFolder temporaryFolder = new TemporaryFolder();
 
   @Test
   public void canFindNodes() throws IOException {
@@ -29,7 +34,22 @@ public class GoogleCloudInfrastructureTest {
     CommandResult result = infra.onNode(node1, new String[] {"echo", "hello"});
 
     assertEquals(0, result.getExitStatus());
-    assertEquals("hello", result.getOutput());
+    assertTrue(result.getOutput().contains("hello"));
+  }
+
+  @Test
+  public void canCopyFilesFromANode()
+      throws IOException, ExecutionException, InterruptedException {
+    GoogleCloudInfrastructure infra = new GoogleCloudInfrastructure(1);
+    Infrastructure.Node node1 = infra.getNodes().iterator().next();
+
+    infra.onNode(node1, new String[] {"mkdir", "-p", "/tmp/foo"});
+    infra.onNode(node1, new String[] {"cp", "/var/log/syslog", "/tmp/foo/file.txt"});
+    File destDirectory = temporaryFolder.newFolder();
+
+    infra.copyFromNode(node1,"/tmp/foo", destDirectory);
+    assertTrue(new File(destDirectory,"foo.tar").exists());
+
   }
 
 }
