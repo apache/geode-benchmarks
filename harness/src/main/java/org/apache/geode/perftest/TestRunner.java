4a36d65c2e891564650a3c0f655e17f969b4d5b0
Dan Smith <upthewaterspout@apache.org>
2018-11-09 13:28:53 -0800

    Adding some log statements

--- harness/src/main/java/org/apache/geode/perftest/TestRunner.java
+++ harness/src/main/java/org/apache/geode/perftest/TestRunner.java
@@ -4,6 +4,9 @@ import java.io.File;
 import java.util.List;
 import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import org.apache.geode.perftest.infrastructure.InfraManager;
 import org.apache.geode.perftest.infrastructure.Infrastructure;
 import org.apache.geode.perftest.jvms.JVMManager;
@@ -18,6 +21,8 @@ import org.apache.geode.perftest.jvms.RemoteJVMs;
  * a declarative fashion and then execute them this runner.
  */
 public class TestRunner {
+  Logger logger = LoggerFactory.getLogger(TestRunner.class);
+
 
   private final InfraManager infraManager;
   private final JVMManager jvmManager;
@@ -36,15 +41,20 @@ public class TestRunner {
 
       Map<String, Integer> roles = config.getRoles();
 
-      //Map roles to nodes and get number of JVMs per node
-
+      logger.info("Lauching JVMs...");
       //launch JVMs in parallel, hook them up
       RemoteJVMs remoteJVMs = jvmManager.launch(infra, roles);
 
+      logger.info("Starting before tasks...");
       runTasks(config.getBefore(), remoteJVMs);
+
+      logger.info("Starting workload tasks...");
       runTasks(config.getWorkload(), remoteJVMs);
+
+      logger.info("Starting after tasks...");
       runTasks(config.getAfter(), remoteJVMs);
 
+      logger.info("Copying results...");
       File outputDir = new File("output");
       int nodeId = 0;
       for(Infrastructure.Node node : infra.getNodes()) {
