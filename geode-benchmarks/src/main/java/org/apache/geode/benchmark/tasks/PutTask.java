a1d71feb0e9c658989c418b77d5b87e429e01176
Dan Smith <upthewaterspout@apache.org>
2018-11-13 16:16:25 -0800

    Adding a README

--- geode-benchmarks/src/main/java/org/apache/geode/benchmark/tasks/PutTask.java
+++ geode-benchmarks/src/main/java/org/apache/geode/benchmark/tasks/PutTask.java
@@ -3,6 +3,7 @@ package org.apache.geode.benchmark.tasks;
 import java.io.Serializable;
 import java.util.Map;
 
+import org.yardstickframework.BenchmarkConfiguration;
 import org.yardstickframework.BenchmarkDriverAdapter;
 
 import org.apache.geode.cache.Region;
@@ -13,10 +14,17 @@ import org.apache.geode.perftest.TestContext;
 
 public class PutTask extends BenchmarkDriverAdapter implements Serializable {
 
+  private Region<Object, Object> region;
+
+  @Override
+  public void setUp(BenchmarkConfiguration cfg) throws Exception {
+    super.setUp(cfg);
+    ClientCache cache = ClientCacheFactory.getAnyInstance();
+    region = cache.getRegion("region");
+  }
+
   @Override
   public boolean test(Map<Object, Object> ctx) throws Exception {
-    ClientCache cache = ClientCacheFactory.getAnyInstance();
-    Region<Object, Object> region = cache.getRegion("region");
     region.put(1,2);
     return true;
   }
