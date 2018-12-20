6d094d266bec98ea057ad11721f0c004935e757c
Brian Rowe <browe@pivotal.io>
2018-12-20 12:07:49 -0800

    96 threads with thread local true

--- harness/src/main/java/org/apache/geode/perftest/WorkloadConfig.java
+++ harness/src/main/java/org/apache/geode/perftest/WorkloadConfig.java
@@ -32,7 +32,7 @@ import org.apache.geode.perftest.yardstick.YardstickTask;
 public class WorkloadConfig implements Serializable {
   long durationSeconds = 1;
   long warmupSeconds = 0;
-  int threads = 64;
+  int threads = 96;
 
   public WorkloadConfig() {}
 
