e900c0b9e5b09610ed7059469b998d3e33b047d1
Sean Goller <sean@goller.net>
2019-05-10 12:02:04 -0700

    Fix expire_clusters to actually do that. (#81)

--- infrastructure/scripts/aws/expire_clusters.sh
+++ infrastructure/scripts/aws/expire_clusters.sh
@@ -19,65 +19,10 @@
 
 set -e
 
-TAG=
-COUNT=
-CI=
-
-while :; do
-  case $1 in
-    -t|--tag )
-      if [ "$2" ]; then
-        TAG=$2
-        shift
-      else
-        echo 'ERROR: "--tag" requires a non-empty argument.'
-        exit 1
-      fi
-      ;;
-    -c|--count )
-      if [ "$2" ]; then
-        COUNT=$2
-        shift
-      else
-        echo 'ERROR: "--count" requires a non-empty argument.'
-        exit 1
-      fi
-      ;;
-    --ci )
-      CI=1
-      ;;
-    -h|--help|-\? )
-      echo "Usage: $(basename "$0") -t tag -c 4 [options ...] [-- arguments ...]"
-      echo "Options:"
-      echo "-t|--tag : Cluster tag"
-      echo "-c|--count : The number of instances to start"
-      echo "--ci : Set if starting instances for Continuous Integration"
-      echo "-- : All subsequent arguments are passed to the benchmark task as arguments."
-      echo "-h|--help : This help message"
-      exit 1
-      ;;
-    -- )
-      shift
-      break 2
-      ;;
-    -?* )
-      printf 'Invalid option: %s\n' "$1" >&2
-      break
-      ;;
-    * )
-      break
-  esac
-  shift
-done
-
 if [[ -z "${AWS_ACCESS_KEY_ID}" ]]; then
   export AWS_PROFILE="geode-benchmarks"
 fi
 
-if [ -z "${CI}" ]; then
-  CI=0
-fi
-
 pushd ../../../
-./gradlew launchCluster -Pci=${CI} --args "${TAG} ${COUNT}"
+./gradlew expireClusters
 popd
