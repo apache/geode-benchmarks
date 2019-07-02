3ac8f2693d33cbf0138e98dce21cbe2275d82fed
Jacob Barrett <jbarrett@pivotal.io>
2019-07-02 12:50:29 -0700

    Adds putAll benchmarks (#79)

---
+++ geode-benchmarks/src/main/java/org/apache/geode/benchmark/tasks/PutAllTask.java
@@ -0,0 +1,62 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.geode.benchmark.tasks;
+
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ThreadLocalRandom;
+
+import benchmark.geode.data.Portfolio;
+import org.yardstickframework.BenchmarkConfiguration;
+import org.yardstickframework.BenchmarkDriverAdapter;
+
+import org.apache.geode.cache.Region;
+import org.apache.geode.cache.client.ClientCache;
+import org.apache.geode.cache.client.ClientCacheFactory;
+
+public class PutAllTask extends BenchmarkDriverAdapter implements Serializable {
+
+  private final long keyRange;
+  private final int batchSize;
+
+  private Region<Object, Object> region;
+
+  public PutAllTask(long keyRange, int batchSize) {
+    this.keyRange = keyRange;
+    this.batchSize = batchSize;
+  }
+
+  @Override
+  public void setUp(BenchmarkConfiguration cfg) throws Exception {
+    super.setUp(cfg);
+    ClientCache cache = ClientCacheFactory.getAnyInstance();
+    region = cache.getRegion("region");
+  }
+
+  @Override
+  public boolean test(Map<Object, Object> ctx) {
+    long key = ThreadLocalRandom.current().nextLong(0, this.keyRange);
+    HashMap<Object, Object> batch = new HashMap<>(batchSize);
+    for (int i = 0; i < batchSize; i++) {
+      batch.put(key, new Portfolio(key));
+    }
+    region.putAll(batch);
+    return true;
+  }
+}
