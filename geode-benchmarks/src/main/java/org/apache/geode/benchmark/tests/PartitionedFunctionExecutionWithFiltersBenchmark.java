80a93d503668c3db1f3c32c359f1a415d1fed88f
Kamilla Aslami <kaslami@pivotal.io>
2019-08-21 13:13:51 -0700

    Increased number of threads

--- geode-benchmarks/src/main/java/org/apache/geode/benchmark/tests/PartitionedFunctionExecutionWithFiltersBenchmark.java
+++ geode-benchmarks/src/main/java/org/apache/geode/benchmark/tests/PartitionedFunctionExecutionWithFiltersBenchmark.java
@@ -33,6 +33,7 @@ public class PartitionedFunctionExecutionWithFiltersBenchmark
   @Override
   public TestConfig configure() {
     TestConfig config = super.configure();
+    config.threads(Runtime.getRuntime().availableProcessors() * 4);
     config.workload(new ExecuteFilteredFunction(getKeyRange()), CLIENT);
     return config;
   }
