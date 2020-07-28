76bbeccf9f10398dde9cfb287d43cf6e758bab24
Jacob Barrett <jbarrett@pivotal.io>
2020-07-28 09:14:34 -0700

    Revert "Fixes for old releases."
    This reverts commit 2934a799

--- geode-benchmarks/src/main/java/org/apache/geode/benchmark/tasks/StartLocatorSNI.java
+++ geode-benchmarks/src/main/java/org/apache/geode/benchmark/tasks/StartLocatorSNI.java
@@ -17,10 +17,13 @@
 
 package org.apache.geode.benchmark.tasks;
 
+import static org.apache.geode.benchmark.tasks.DefineHostNamingsOffPlatformTask.getOffPlatformHostName;
 
 import java.io.IOException;
+import java.net.InetAddress;
 import java.util.Properties;
 
+import org.apache.geode.distributed.LocatorLauncher;
 import org.apache.geode.perftest.TestContext;
 
 public class StartLocatorSNI extends StartLocator {
@@ -32,13 +35,13 @@ public class StartLocatorSNI extends StartLocator {
   @Override
   protected void startLocator(final Properties properties, final int locatorPort,
       final TestContext context) throws IOException {
-    // new LocatorLauncher.Builder()
-    // .set(properties)
-    // .setPort(locatorPort)
-    // .setHostnameForClients(
-    // getOffPlatformHostName(context, InetAddress.getLocalHost()))
-    // .build()
-    // .start();
+    new LocatorLauncher.Builder()
+        .set(properties)
+        .setPort(locatorPort)
+        .setHostnameForClients(
+            getOffPlatformHostName(context, InetAddress.getLocalHost()))
+        .build()
+        .start();
   }
 
 }
