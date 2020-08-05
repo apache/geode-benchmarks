9ae64fddf5397a17951b8fd37f02109125d79b51
Jacob Barrett <jbarrett@pivotal.io>
2020-08-04 19:26:09 -0700

    Cipher TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256

--- geode-benchmarks/src/main/java/org/apache/geode/benchmark/parameters/GeodeProperties.java
+++ geode-benchmarks/src/main/java/org/apache/geode/benchmark/parameters/GeodeProperties.java
@@ -91,6 +91,7 @@ public class GeodeProperties {
 
   public static Properties withSsl(Properties properties) {
     properties.setProperty(SSL_ENABLED_COMPONENTS, ALL);
+    properties.setProperty(SSL_CIPHERS, "TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256");
     return properties;
   }
 
