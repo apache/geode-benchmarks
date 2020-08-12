58220cede67ce4c834527d755c20728a11d07e3f
Robert Houghton <rhoughton@pivotal.io>
2020-08-12 10:43:46 -0700

    Revert "allow AMI picking with arbitrary username accounts (#132)"
    This reverts commit 3e3bdd3a371e400db0ce2ce2d736ab06f9244ed1.

--- infrastructure/scripts/aws/destroy_cluster.sh
+++ infrastructure/scripts/aws/destroy_cluster.sh
@@ -17,15 +17,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-
-SOURCE="${BASH_SOURCE[0]}"
-while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
-  SCRIPTDIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
-  SOURCE="$(readlink "$SOURCE")"
-  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
-done
-SCRIPTDIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
-
 TAG=
 CI=
 
@@ -72,6 +63,6 @@ if [ -z "${CI}" ]; then
   CI=0
 fi
 
-pushd "${SCRIPTDIR}/../../../"
+pushd ../../../
 ./gradlew destroyCluster -Pci=${CI} --args "${TAG}"
 popd
