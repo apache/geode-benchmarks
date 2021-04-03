689b5e904fdb6491821121e69f8817429c1091f5
Jacob Barrett <jbarrett@pivotal.io>
2021-04-03 06:30:03 -0700

    Run remote shells in parallel.

--- infrastructure/scripts/aws/run_on_cluster.sh
+++ infrastructure/scripts/aws/run_on_cluster.sh
@@ -60,10 +60,12 @@ if [[ -z "${AWS_ACCESS_KEY_ID}" ]]; then
   export AWS_PROFILE="geode-benchmarks"
 fi
 
-SSH_OPTIONS="-o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i ~/.geode-benchmarks/${TAG}-privkey.pem"
+SSH_OPTIONS="-o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -o LogLevel=ERROR -i ~/.geode-benchmarks/${TAG}-privkey.pem"
 HOSTS=`aws ec2 describe-instances --query 'Reservations[*].Instances[*].PublicIpAddress' --filter "Name=tag:geode-benchmarks,Values=${TAG}" --output text`
 
 for host in ${HOSTS}; do
   echo "Executing on ${host}."
-  ssh ${SSH_OPTIONS} geode@${host} "$@"
+  ssh ${SSH_OPTIONS} geode@${host} "$@" &
 done
+
+wait
