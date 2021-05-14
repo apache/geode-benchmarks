66a23e9096882fe6ce9591cd27640ea7b50711c9
Jacob Barrett <jbarrett@pivotal.io>
2021-05-14 16:43:28 -0700

    Refactor package to make test pattern matching easier by package.

---
+++ geode-benchmarks/src/main/java/org/apache/geode/benchmark/redis/tests/RedisBenchmark.java
@@ -0,0 +1,152 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more contributor license
+ * agreements. See the NOTICE file distributed with this work for additional information regarding
+ * copyright ownership. The ASF licenses this file to You under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the License. You may obtain a
+ * copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package org.apache.geode.benchmark.redis.tests;
+
+import static com.google.common.base.Strings.isNullOrEmpty;
+import static java.lang.Long.getLong;
+import static org.apache.geode.benchmark.Config.after;
+import static org.apache.geode.benchmark.Config.before;
+import static org.apache.geode.benchmark.redis.tests.RedisBenchmark.RedisClientImplementation.Jedis;
+import static org.apache.geode.benchmark.redis.tests.RedisBenchmark.RedisClusterImplementation.Geode;
+import static org.apache.geode.benchmark.redis.tests.RedisBenchmark.RedisClusterImplementation.Manual;
+import static org.apache.geode.benchmark.tests.GeodeBenchmark.WITH_MAX_KEY;
+import static org.apache.geode.benchmark.tests.GeodeBenchmark.WITH_MIN_KEY;
+import static org.apache.geode.benchmark.topology.Roles.CLIENT;
+
+import org.junit.jupiter.api.Test;
+
+import org.apache.geode.benchmark.LongRange;
+import org.apache.geode.benchmark.redis.tasks.FlushDbTask;
+import org.apache.geode.benchmark.redis.tasks.JedisClientManager;
+import org.apache.geode.benchmark.redis.tasks.LettuceClientManager;
+import org.apache.geode.benchmark.redis.tasks.RedisClientManager;
+import org.apache.geode.benchmark.redis.tasks.StartRedisClient;
+import org.apache.geode.benchmark.redis.tasks.StopRedisClient;
+import org.apache.geode.benchmark.redis.topology.GeodeTopology;
+import org.apache.geode.benchmark.redis.topology.ManualRedisTopology;
+import org.apache.geode.benchmark.redis.topology.RedisTopology;
+import org.apache.geode.benchmark.tests.AbstractPerformanceTest;
+import org.apache.geode.benchmark.tests.GeodeBenchmark;
+import org.apache.geode.perftest.TestConfig;
+import org.apache.geode.perftest.TestRunners;
+
+public class RedisBenchmark extends AbstractPerformanceTest {
+
+  public static final String WITH_REDIS_CLIENT_PROPERTY = "benchmark.withRedisClient";
+  public static final String WITH_REDIS_CLUSTER_PROPERTY = "benchmark.withRedisCluster";
+
+  public static final String REDIS_SERVERS_ATTRIBUTE = "RedisBenchmark.Servers";
+
+  public enum RedisClientImplementation {
+    Jedis,
+    Lettuce;
+
+    public static RedisClientImplementation valueOfIgnoreCase(final String name) {
+      for (RedisClientImplementation redisClientImplementation : RedisClientImplementation
+          .values()) {
+        if (redisClientImplementation.name().equalsIgnoreCase(name)) {
+          return redisClientImplementation;
+        }
+      }
+      throw new IllegalArgumentException("Unknown Redis client implementation: " + name);
+    }
+  }
+
+  public enum RedisClusterImplementation {
+    Geode,
+    Redis,
+    Manual;
+
+    public static RedisClusterImplementation valueOfIgnoreCase(final String name) {
+      for (RedisClusterImplementation redisClusterImplementation : RedisClusterImplementation
+          .values()) {
+        if (redisClusterImplementation.name().equalsIgnoreCase(name)) {
+          return redisClusterImplementation;
+        }
+      }
+      throw new IllegalArgumentException("Unknown Redis cluster implementation: " + name);
+    }
+  }
+
+  protected transient RedisClientManager redisClientManager;
+
+  protected LongRange keyRange =
+      new LongRange(getLong(WITH_MIN_KEY, 0), getLong(WITH_MAX_KEY, 1000000));
+
+  public void setKeyRange(final LongRange keyRange) {
+    this.keyRange = keyRange;
+  }
+
+  @Test
+  public void run() throws Exception {
+    TestRunners.defaultRunner().runTest(this);
+  }
+
+  @Override
+  public TestConfig configure() {
+    TestConfig config = GeodeBenchmark.createConfig();
+
+    switch (getRedisClusterImplementation()) {
+      case Redis:
+        RedisTopology.configure(config);
+        break;
+      case Geode:
+        GeodeTopology.configure(config);
+        break;
+      case Manual:
+        ManualRedisTopology.configure(config);
+        break;
+    }
+
+    switch (getRedisClientImplementation()) {
+      case Jedis:
+        redisClientManager = new JedisClientManager();
+        break;
+      case Lettuce:
+        redisClientManager = new LettuceClientManager();
+        break;
+    }
+
+    before(config, new StartRedisClient(redisClientManager), CLIENT);
+
+    if (Manual == getRedisClusterImplementation()) {
+      before(config, new FlushDbTask(redisClientManager), CLIENT);
+    }
+
+    after(config, new StopRedisClient(redisClientManager), CLIENT);
+
+    return config;
+  }
+
+  private RedisClientImplementation getRedisClientImplementation() {
+    final String sniProp = System.getProperty(WITH_REDIS_CLIENT_PROPERTY);
+    if (isNullOrEmpty(sniProp)) {
+      return Jedis;
+    }
+
+    return RedisClientImplementation.valueOfIgnoreCase(sniProp);
+  }
+
+  private RedisClusterImplementation getRedisClusterImplementation() {
+    final String sniProp = System.getProperty(WITH_REDIS_CLUSTER_PROPERTY);
+    if (isNullOrEmpty(sniProp)) {
+      return Geode;
+    }
+
+    return RedisClusterImplementation.valueOfIgnoreCase(sniProp);
+  }
+
+}
---
+++ geode-benchmarks/src/main/java/org/apache/geode/benchmark/redis/topology/RedisTopology.java
@@ -0,0 +1,57 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more contributor license
+ * agreements. See the NOTICE file distributed with this work for additional information regarding
+ * copyright ownership. The ASF licenses this file to You under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the License. You may obtain a
+ * copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package org.apache.geode.benchmark.redis.topology;
+
+import static java.lang.Integer.getInteger;
+import static org.apache.geode.benchmark.Config.after;
+import static org.apache.geode.benchmark.Config.before;
+import static org.apache.geode.benchmark.Config.role;
+import static org.apache.geode.benchmark.tests.GeodeBenchmark.WITH_REPLICAS;
+import static org.apache.geode.benchmark.topology.Roles.CLIENT;
+import static org.apache.geode.benchmark.topology.Roles.LOCATOR;
+import static org.apache.geode.benchmark.topology.Roles.SERVER;
+
+import org.apache.geode.benchmark.redis.tasks.CreateRedisCluster;
+import org.apache.geode.benchmark.redis.tasks.InitRedisServersAttribute;
+import org.apache.geode.benchmark.redis.tasks.StartRedisServer;
+import org.apache.geode.benchmark.redis.tasks.StopRedisServer;
+import org.apache.geode.benchmark.topology.Topology;
+import org.apache.geode.perftest.TestConfig;
+
+/**
+ * Redis running in containers on the provided servers.
+ *
+ * Locators hosts are wasted so that Redis server placement happens on the same hosts as Geode.
+ */
+public class RedisTopology extends Topology {
+  private static final int NUM_LOCATORS = Integer.getInteger(WITH_LOCATOR_COUNT_PROPERTY, 1);
+  private static final int NUM_SERVERS = Integer.getInteger(WITH_SERVER_COUNT_PROPERTY, 6);
+  private static final int NUM_CLIENTS = Integer.getInteger(WITH_CLIENT_COUNT_PROPERTY, 1);
+
+  public static void configure(TestConfig config) {
+    role(config, LOCATOR, NUM_LOCATORS);
+    role(config, SERVER, NUM_SERVERS);
+    role(config, CLIENT, NUM_CLIENTS);
+
+    configureCommon(config);
+
+    before(config, new StartRedisServer(), SERVER);
+    before(config, new CreateRedisCluster(getInteger(WITH_REPLICAS, 1)), CLIENT);
+    before(config, new InitRedisServersAttribute(), CLIENT);
+
+    after(config, new StopRedisServer(), SERVER);
+  }
+}
